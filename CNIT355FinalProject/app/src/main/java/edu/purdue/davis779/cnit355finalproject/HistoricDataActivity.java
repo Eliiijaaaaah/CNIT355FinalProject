7c70de862c98b20d84e04e894ee3927c2e6bf412
TBow16 <trevdog16@gmail.com>
2018-11-30 20:43:10 -0500

    Update HistoricDataActivity.java
    temp graph data

--- CNIT355FinalProject/app/src/main/java/edu/purdue/davis779/cnit355finalproject/HistoricDataActivity.java
+++ CNIT355FinalProject/app/src/main/java/edu/purdue/davis779/cnit355finalproject/HistoricDataActivity.java
@@ -3,11 +3,25 @@ package edu.purdue.davis779.cnit355finalproject;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.jjoe64.graphview.GraphView;
+import com.jjoe64.graphview.series.DataPoint;
+import com.jjoe64.graphview.series.LineGraphSeries;
+
 public class HistoricDataActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_historic_data);
+
+        GraphView  graph = (GraphView) findViewById(R.id.graph);
+        LineGraphSeries<DataPoint> series = new LineGraphSeries<>(new DataPoint[] {
+                new DataPoint(0, 1),
+                new DataPoint(1, 5),
+                new DataPoint(2, 3),
+                new DataPoint(3, 2),
+                new DataPoint(4, 6)
+        });
+        graph.addSeries(series);
     }
 }
