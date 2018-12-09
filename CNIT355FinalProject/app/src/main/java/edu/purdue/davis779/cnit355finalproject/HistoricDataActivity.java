1510cfb2fcb0362a6eded0cac6eeb39d10e3f873
Elijah T Davis <elijahit@purdue.edu>
2018-12-09 15:37:04 -0500

    Graphing awesomeness

--- CNIT355FinalProject/app/src/main/java/edu/purdue/davis779/cnit355finalproject/HistoricDataActivity.java
+++ CNIT355FinalProject/app/src/main/java/edu/purdue/davis779/cnit355finalproject/HistoricDataActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.SeekBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -20,13 +21,17 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Calendar;
 
 public class HistoricDataActivity extends AppCompatActivity implements Runnable {
     String coin;
     Thread thread;
     String msg;
+    GraphView graph;
+    int size = -9;
 
     private static HistoricDataActivity parent;
 
@@ -45,15 +50,27 @@ public class HistoricDataActivity extends AppCompatActivity implements Runnable 
 
         refresh();
 
-        GraphView  graph = (GraphView) findViewById(R.id.graph);
-        LineGraphSeries<DataPoint> series = new LineGraphSeries<>(new DataPoint[] {
-                new DataPoint(0, 1),
-                new DataPoint(1, 5),
-                new DataPoint(2, 3),
-                new DataPoint(3, 2),
-                new DataPoint(4, 6)
+        graph = (GraphView) findViewById(R.id.graph);
+
+        SeekBar seekbar = findViewById(R.id.seekBar);
+        seekbar.setMax(365);
+        seekbar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                size = progress*-1;
+                refresh();
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
         });
-        graph.addSeries(series);
     }
 
     public void endActivity(View view){
@@ -126,6 +143,79 @@ public class HistoricDataActivity extends AppCompatActivity implements Runnable 
 
             TextView txtMaxEx = findViewById(R.id.txtMaxEx);
             txtMaxEx.setText(maxExchange);
+        } catch (Exception e) {
+            msg = e.getMessage();
+
+            //Reference: https://stackoverflow.com/questions/17379002/java-lang-runtimeexception-cant-create-handler-inside-thread-that-has-not-call
+            parent.runOnUiThread(new Runnable() {
+                public void run() {
+                    Toast.makeText(parent.getBaseContext(), msg, Toast.LENGTH_LONG).show();
+                }
+            });
+        } finally {
+            if (inStream != null) {
+                try {
+                    // this will close the bReader as well
+                    inStream.close();
+                } catch (IOException ignored) {
+                }
+            }
+            if (urlConnection != null) {
+                urlConnection.disconnect();
+            }
+        }
+
+        try {
+            SimpleDateFormat dateFormat = new SimpleDateFormat("YYYY-MM-dd'T'HH:MM:ss");
+            Calendar calendar = Calendar.getInstance();
+            String end = dateFormat.format(calendar.getTime()) + "Z";
+            calendar.add(Calendar.DAY_OF_YEAR, size);
+            String start = dateFormat.format(calendar.getTime()) + "Z";
+            //start = "2018-12-08T00:00:00Z";
+
+            url = new URL("https://api.nomics.com/v1/candles?key=1c5ffff4fc72b2138210c4a9ea578dc4&currency=" + coin + "&start=" + start + "&end=" + end);
+            urlConnection = (HttpURLConnection) url.openConnection();
+            urlConnection.setRequestMethod("GET");
+            urlConnection.setDoOutput(true);
+            urlConnection.setDoInput(true);
+            urlConnection.connect();
+            inStream = urlConnection.getInputStream();
+            BufferedReader bReader = new BufferedReader(new InputStreamReader(inStream));
+            String temp, response = "";
+            while ((temp = bReader.readLine()) != null) {
+                response += temp;
+            }
+            object = (JSONArray) new JSONTokener(response).nextValue();
+
+            //double min = Collections.min(Prices).doubleValue();
+            //double max = Collections.max(Prices).doubleValue();
+
+            DataPoint[] datapoints = new DataPoint[object.length()];
+
+            for (int count = 0; count < object.length(); count++){
+                String low = object.getJSONObject(count).getString("low").toString();
+                Double dPrice = Double.parseDouble(low);
+                datapoints[count] = new DataPoint(count, dPrice);
+            }
+            LineGraphSeries<DataPoint> series = new LineGraphSeries<>(datapoints);
+            graph.addSeries(series);
+
+            for (int count = 0; count < object.length(); count++){
+                String high = object.getJSONObject(count).getString("high").toString();
+                Double dPrice = Double.parseDouble(high);
+                datapoints[count] = new DataPoint(count, dPrice);
+            }
+
+            series = new LineGraphSeries<>(datapoints);
+            graph.removeAllSeries();
+            graph.addSeries(series);
+
+            msg = start;
+            parent.runOnUiThread(new Runnable() {
+                public void run() {
+                    Toast.makeText(parent.getBaseContext(), msg, Toast.LENGTH_LONG).show();
+                }
+            });
         } catch (Exception e) {
             msg = e.getMessage();
 
